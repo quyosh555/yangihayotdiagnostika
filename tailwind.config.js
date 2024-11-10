5a2c2a9884f64f12bfc17967868f7647e8f73dba
quyosh555 <vatantibbiyotmarkazi@gmail.com>
2024-11-10 23:05:24 +0500

    Change layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,16 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1170px',
+      },
+    },
   },
   plugins: [],
 }
